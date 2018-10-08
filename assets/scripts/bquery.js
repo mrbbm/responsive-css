6a2488643b19ff59cc03c0a89ea3d7aaa9330561
B.Balamanigandan <bbm@bbminfo.com>
2018-10-08 06:01:13 +0530

    Update in code snippet

--- assets/scripts/bquery.js
+++ assets/scripts/bquery.js
@@ -1,27 +1,59 @@
-$(function () {
+$(function() {
   $('[data-toggle="tooltip"]').tooltip();
   $('[data-toggle="popover"]').popover();
 });
 
 function toggleSnipet(elementId) {
-  changeToggle(elementId, 'code-snip-noline')
+  const snippetName = "selectable" + elementId;
+  const toolName = "toggle" + elementId;
+  const counterName = "counter" + elementId;
+  const countdown = 20;
+  $("#" + toolName).hide();
+  $("#" + counterName).show();
+  selectText(snippetName);
+  $("#" + counterName).text(countdown + " seconds");
+  const counterObj = counter(countdown - 1, false);
+  changeToggle(snippetName, "code-snip-noline");
+  let interval = setInterval(function() {
+    $("#" + counterName).text(counterObj() + " seconds");
+  }, 1000);
+  setTimeout(function() {
+    clearInterval(interval);
+    $("#" + counterName).text("");
+    $("#" + toolName).show();
+    $("#" + counterName).hide();
+    changeToggle(snippetName, "code-snip-line");
+  }, countdown * 1000);
+}
 
-  setTimeout(function()
-  { 
-    document.getElementById(elementId).className = "mystyle";
-    changeToggle(elementId, 'code-snip-line')
-  }, 10000);
+function selectText(containerid) {
+  containerid = "selectable1";
+  if (document.selection) {
+    // IE
+    var range = document.body.createTextRange();
+    range.moveToElementText(document.getElementById(containerid));
+    range.select();
+  } else if (window.getSelection) {
+    var range = document.createRange();
+    range.selectNode(document.getElementById(containerid));
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+  }
+}
 
+function counter(counter, flag) {
+  let i = counter;
+  return function() {
+    return flag ? i++ : i--;
+  };
 }
 
 function changeToggle(elementId, className) {
-  var $element = $('#' + elementId);
-  var $children = $element.children('li');
-  $children.each(function(idx, li)
-  {
+  var $element = $("#" + elementId);
+  var $children = $element.children("li");
+  $children.each(function(idx, li) {
     var codeItem = $(li);
-    codeItem.attr('class', className);
-    console.log(codeItem)
+    codeItem.attr("class", className);
   });
 }
 
